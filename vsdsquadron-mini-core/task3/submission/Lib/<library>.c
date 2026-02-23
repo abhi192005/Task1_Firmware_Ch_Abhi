32676463a2f2917cee3cf0f8b6235735a4baf806
abhi192005 <abhisathvikchigurupati@gmail.com>
2026-02-23 21:03:15 +0530

    Create <library>.c

---
+++ vsdsquadron-mini-core/task3/submission/Lib/<library>.c
@@ -0,0 +1,41 @@
+#include "ch32v00x.h"
+#include "gpio.h"
+
+void gpio_init(int pin, int mode)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+
+    GPIO_InitTypeDef gpio;
+    gpio.GPIO_Pin = pin;
+    gpio.GPIO_Speed = GPIO_Speed_50MHz;
+
+    if (mode == GPIO_OUTPUT)
+        gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+    else
+        gpio.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+
+    GPIO_Init(GPIOD, &gpio);
+}
+
+void gpio_set(int pin)
+{
+    GPIO_SetBits(GPIOD, pin);
+}
+
+void gpio_clear(int pin)
+{
+    GPIO_ResetBits(GPIOD, pin);
+}
+
+void gpio_toggle(int pin)
+{
+    if (GPIO_ReadOutputDataBit(GPIOD, pin))
+        GPIO_ResetBits(GPIOD, pin);
+    else
+        GPIO_SetBits(GPIOD, pin);
+}
+
+int gpio_read(int pin)
+{
+    return GPIO_ReadInputDataBit(GPIOD, pin);
+}
