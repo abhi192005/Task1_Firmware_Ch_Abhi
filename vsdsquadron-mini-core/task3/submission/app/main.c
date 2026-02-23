766a45f92e39d7eb6ebd31857c8ec1b874a965b8
abhi192005 <abhisathvikchigurupati@gmail.com>
2026-02-23 22:16:36 +0530

    Update main.c

--- vsdsquadron-mini-core/task3/submission/app/main.c
+++ vsdsquadron-mini-core/task3/submission/app/main.c
@@ -2,44 +2,32 @@
 #include <debug.h>
 #include "gpio.h"
 
-#define LED_PIN    GPIO_Pin_6   // PD6 onboard LED
-#define BUTTON_PIN GPIO_Pin_0   // PD0 external button
+#define LED_PIN     GPIO_Pin_6  // PD6 LED
+#define BUTTON_PIN  GPIO_Pin_0  // PD0 Button
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 
-int main(void)
-{
+int main(void) {
     SystemCoreClockUpdate();
     Delay_Init();
     USART_Printf_Init(115200);
 
-    printf("GPIO Button Toggle Demo Started\r\n");
-
-    /* Initialize GPIOs using library */
     gpio_init(LED_PIN, GPIO_OUTPUT);
     gpio_init(BUTTON_PIN, GPIO_INPUT);
 
-    
+    printf("=== Task-3 Option-1: Advanced GPIO Demo ===\r\n");
+    printf("PD0 Button -> Toggle PD6 LED\r\n");
+    printf("Press button 10x for evidence logs\r\n");
 
-    while (1)
-    {
-        /* Button pressed (active low) */
-        if (gpio_read(BUTTON_PIN) == 0)
-        {
-            gpio_toggle(LED_PIN);   // toggle software state
-
-           
-              // simple debounce
+    while (1) {
+        if (gpio_read_debounced(BUTTON_PIN)) {
+            gpio_toggle(LED_PIN);
+            printf("LED TOGGLED: PD6=%s | Button=PD0\r\n", 
+                   gpio_read(LED_PIN) ? "ON " : "OFF");
         }
-        else
-            gpio_clear(LED_PIN);
     }
 }
 
 void NMI_Handler(void) {}
-
-void HardFault_Handler(void)
-{
-    while (1);
-}
+void HardFault_Handler(void) { while (1); }
